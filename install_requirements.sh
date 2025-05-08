7d38324759fd2e34a5dc761c6f1e6b186d403939
Giovanni Bartolomeo <giobarty@gmail.com>
2025-05-08 13:16:29 +0200

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl
+apt-get update -y
+apt-get install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -12,16 +12,16 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
+apt-get update -y
 
 groupadd docker
 usermod -aG docker $USER
 
 # Install pip
-apt install python3-pip
+apt install -y python3-pip
 
 # Install venv
-apt install python3-venv
+apt install -y python3-venv
 
 # Install ifstat
-apt-get install ifstat
+apt-get install -y ifstat
