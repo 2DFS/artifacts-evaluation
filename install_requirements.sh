a596b37b1749745ff8f7a1f4e987c39caa05817f
Giovanni Bartolomeo <giobarty@gmail.com>
2025-05-08 13:53:22 +0200

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -14,6 +14,10 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
 
+# Install docker 
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Sudoless docker
 groupadd docker
 usermod -aG docker $USER
 
